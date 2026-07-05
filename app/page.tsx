94fc0174ccc6c469b3034bec5421700f11b44c88
Johnny Cage <thespammingbott@gmail.com>
2026-07-05 18:05:51 +0100

    configure tailwlind setup and theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <h1 className="text-3xl text-violet-700 font-black">Welcome to Next.js 👋</h1>
+            <h1 className="text-3xl text-light-500 font-black">Welcome to Next.js 👋</h1>
         </div>
     );
 };
