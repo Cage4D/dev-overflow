e3240725f251657a5ba3b2b7fb64dc99ec0d8c20
Cage4D <thespammingbott@gmail.com>
2026-09-01 09:49:20 +0100

    Implement Interaction Model

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,39 +1,42 @@
-import mongoose, { Mongoose } from "mongoose"
+import mongoose, { Mongoose } from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-    throw new Error("MONGODB_URI not defined")
+  throw new Error("MONGODB_URI not defined");
 }
 
 interface MongooseCache {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
 declare global {
-    var mongoose: MongooseCache;
+  var mongoose: MongooseCache;
 }
 
 let cached = global.mongoose;
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null }
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 const dbConnect = async (): Promise<Mongoose> => {
-    if (cached.conn) return cached.conn;
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI, {
-            dbName: "DevFlow",
-        }).then((result) => {
-            console.log("Connected to MongoDB")
-            return result;
-        }).catch((error) => {
-            console.error("Error connecting to MongoDB", error);
-            throw error;
-        })
-    }
-    cached.conn = await cached.promise;
-    return cached.conn;
-}
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "DevFlow",
+      })
+      .then((result) => {
+        console.log("Connected to MongoDB");
+        return result;
+      })
+      .catch((error) => {
+        console.error("Error connecting to MongoDB", error);
+        throw error;
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
 
 export default dbConnect;
