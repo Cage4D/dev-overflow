92abdc5e12c96bded503cabde786e716e2856c08
Cage4D <thespammingbott@gmail.com>
2026-09-22 20:49:02 +0100

    fix: store question author as ObjectId so author filters match seed data

--- database/question.model.ts
+++ database/question.model.ts
@@ -9,7 +9,7 @@ export interface IQuestion {
   upvotes: number;
   downvotes: number;
   answers: number;
-  author: string;
+  author: Types.ObjectId;
 }
 
 export interface IQuestionDoc extends IQuestion, Document {}
@@ -22,7 +22,7 @@ const QuestionSchema = new Schema<IQuestion>(
     upvotes: { type: Number, default: 0 },
     downvotes: { type: Number, default: 0 },
     answers: { type: Number, default: 0 },
-    author: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
   { timestamps: true },
 );
